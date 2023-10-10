b79323c6662876d5655516bfeb412430639f48e0
Emily Krohn <emilykrohn2@gmail.com>
2023-10-09 21:21:01 -0700

    Change animation when walk, jump, stand
    Walk isn't working correctly yet but jump and stand are

--- player.gd
+++ player.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -325.0
 const ACCELERATION = 400
 const FRICTION = 1400
 
+var is_walking := false
+var is_jumping := false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -18,13 +21,24 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+		$AnimatedSprite2D.animation = "jump"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
+		is_walking = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		$AnimatedSprite2D.animation = "stand"
+		is_walking = false
+		is_jumping = false
+	
+	if is_jumping:
+		$AnimatedSprite2D.animation = "jump"
+	elif is_walking:
+		$AnimatedSprite2D.animation = "walk"
 
 	move_and_slide()
