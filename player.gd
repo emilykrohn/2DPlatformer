66853a9fa45214441e21657a5f5d558d9674371c
Emily Krohn <emilykrohn2@gmail.com>
2023-10-10 21:01:21 -0700

    Fix jump animation

--- player.gd
+++ player.gd
@@ -17,26 +17,30 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.animation = "jump"
 		
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
 
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.animation = "jump"
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
+		if is_on_floor():
+			$AnimatedSprite2D.animation = "walk"
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		$AnimatedSprite2D.animation = "stand"
+	
+	if not is_on_floor():
+		$AnimatedSprite2D.animation = "jump"
 
 	move_and_slide()
