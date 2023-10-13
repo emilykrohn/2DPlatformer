7178e60e4ac0f02d03c5883e770961020c33863c
Emily Krohn <emilykrohn2@gmail.com>
2023-10-12 21:02:00 -0700

    Player damage delayed by 1 second

--- enemy_1.gd
+++ enemy_1.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var damage_player := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,10 +9,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if damage_player and get_node("../Player").can_take_damage:
+		get_node("../Player").damage_player(1)
+		get_node("../Player").delay_damage()
+		print(get_node("../Player").health)
 
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
-		get_node("../Player").damage_player(1)
-		print(get_node("../Player").health)
+		damage_player = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		damage_player = false
