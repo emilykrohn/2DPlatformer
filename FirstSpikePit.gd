fbddcf11b8b8f55ed64933c69329e97903f28963
Emily Krohn <emilykrohn2@gmail.com>
2023-10-15 20:52:37 -0700

    Player respawns before first pit when fall

--- FirstSpikePit.gd
+++ FirstSpikePit.gd
@@ -12,3 +12,10 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.name == "Player":
 		can_damage = true
+		await get_tree().create_timer(0.1).timeout
+		body.global_position = Vector2(375, -38)
+
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		can_damage = false
