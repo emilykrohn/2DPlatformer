cdd528d50bbd07b2aabf5808767e07b93466a52a
Emily Krohn <emilykrohn2@gmail.com>
2023-10-23 08:24:20 -0700

    Create flying enemy scene

---
+++ flying_enemy.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+var damage_player := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if damage_player and get_node("../Player").can_take_damage:
+		get_node("../Player").damage_player(1)
+		get_node("../Player").delay_damage()
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		damage_player = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		damage_player = false
