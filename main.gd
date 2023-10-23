40d964d33b8be202bb5d6b0b4fe14bf24056abe1
Emily Krohn <emilykrohn2@gmail.com>
2023-10-23 08:37:10 -0700

    Enemy flies up an down
    Damage isn't working anymore

--- main.gd
+++ main.gd
@@ -3,3 +3,4 @@ extends Node
 func _physics_process(delta):
 	const move_speed := 50.0
 	$Enemy1Path/PathFollow2D.progress += move_speed * delta
+	$FlyingEnemy1Path/PathFollow2D.progress += move_speed * delta
