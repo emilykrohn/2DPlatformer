db4aeffe0456dfe6eb7adda2ef02ba7a6af9b54a
Emily Krohn <emilykrohn2@gmail.com>
2023-10-22 19:31:58 -0700

    Coin count on HUD increase on player collision

--- coin.gd
+++ coin.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,4 +14,6 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
+		get_node("../../Player").coins += 1
+		get_node("../../Player/HUD/Coins").text = "Coins: " + str(get_node("../../Player").coins)
 		queue_free()
