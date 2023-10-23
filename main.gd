e33a5be657e331ea9538b2c53207d911f72de899
Emily Krohn <emilykrohn2@gmail.com>
2023-10-22 20:25:10 -0700

    Fixed Zero length interval error

--- main.gd
+++ main.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _physics_process(delta):
 	const move_speed := 50.0
-	$Path2D/PathFollow2D.progress += move_speed * delta
+	$Enemy1Path/PathFollow2D.progress += move_speed * delta
