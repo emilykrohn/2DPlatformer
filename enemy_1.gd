4828008bed456cec0968b46a3904840a6297a2d1
Emily Krohn <emilykrohn2@gmail.com>
2023-10-23 21:20:07 -0700

    Put Enemy paths in a group

--- enemy_1.gd
+++ enemy_1.gd
@@ -8,9 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if damage_player and get_node("../../../Player").can_take_damage:
-		get_node("../../../Player").damage_player(1)
-		get_node("../../../Player").delay_damage()
+	if damage_player and get_node("../../../../Player").can_take_damage:
+		get_node("../../../../Player").damage_player(1)
+		get_node("../../../../Player").delay_damage()
 
 
 func _on_area_2d_body_entered(body):
--- main.gd
+++ main.gd
@@ -1,8 +1,12 @@
 extends Node
 
+var enemy_paths
 func _physics_process(delta):
 	const move_speed := 50.0
-	$Enemy1Path/PathFollow2D.progress += move_speed * delta
-	$FlyingEnemy1Path/PathFollow2D.progress += move_speed * delta
-	$FlyingEnemy2Path/PathFollow2D.progress += move_speed * delta
-	$FlyingEnemy3Path/PathFollow2D.progress += move_speed * delta
+	enemy_paths = get_tree().get_nodes_in_group("EnemyPaths")
+	for enemy in enemy_paths:
+		enemy.get_node("PathFollow2D").progress += move_speed * delta
+	#$Enemy1Path/PathFollow2D.progress += move_speed * delta
+	#$FlyingEnemy1Path/PathFollow2D.progress += move_speed * delta
+	#$FlyingEnemy2Path/PathFollow2D.progress += move_speed * delta
+	#$FlyingEnemy3Path/PathFollow2D.progress += move_speed * delta
