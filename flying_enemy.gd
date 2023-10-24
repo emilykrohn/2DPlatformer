3ffff6dffbff453e05e083427a62a6c0acee1c48
Emily Krohn <emilykrohn2@gmail.com>
2023-10-23 20:37:07 -0700

    Fix crash on player collision with enemy
    Fixed file path to player from enemy

--- flying_enemy.gd
+++ flying_enemy.gd
@@ -10,9 +10,9 @@ func _ready():
 func _process(delta):
 	$AnimatedSprite2D.animation = "fly"
 	$AnimatedSprite2D.play()
-	if damage_player and get_node("../Player").can_take_damage:
-		get_node("../Player").damage_player(1)
-		get_node("../Player").delay_damage()
+	if damage_player and get_node("../../../Player").can_take_damage:
+		get_node("../../../Player").damage_player(1)
+		get_node("../../../Player").delay_damage()
 
 
 func _on_area_2d_body_entered(body):
