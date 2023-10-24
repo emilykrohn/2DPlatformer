fcf6e5dceb368872785804d78d61c4dd0b6348b0
Emily Krohn <emilykrohn2@gmail.com>
2023-10-23 20:47:49 -0700

    Add two more flying enemies to main scene

--- main.gd
+++ main.gd
@@ -4,3 +4,5 @@ func _physics_process(delta):
 	const move_speed := 50.0
 	$Enemy1Path/PathFollow2D.progress += move_speed * delta
 	$FlyingEnemy1Path/PathFollow2D.progress += move_speed * delta
+	$FlyingEnemy2Path/PathFollow2D.progress += move_speed * delta
+	$FlyingEnemy3Path/PathFollow2D.progress += move_speed * delta
